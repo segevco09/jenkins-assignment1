16a54de4f5b6eadf08c36c74c3bbe89d23d94531
Segevc98 <156806005+Segevc98@users.noreply.github.com>
2025-03-24 16:26:45 +0200

    Changed the text inside Segev.java

--- Segev.java
+++ Segev.java
@@ -1,7 +1,7 @@
 public class Segev{
 	public static void main(String []args){
 		for (int i = 1; i <= 10; i++){
-			System.out.println("My name is Segev!" +i);
+			System.out.println("My name is Segev!!" +i);
 			}
 		}
 	}
